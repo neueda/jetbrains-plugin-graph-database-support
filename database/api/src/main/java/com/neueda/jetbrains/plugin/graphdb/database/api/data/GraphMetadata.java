ff63ec5086ee47b3baf80c21f8685f716ea435ec
Dwitry <dwitry@users.noreply.github.com>
2019-09-24 17:03:18 +0300

    Cypher for Gremlin improvements

--- database/api/src/main/java/com/neueda/jetbrains/plugin/graphdb/database/api/data/GraphMetadata.java
+++ database/api/src/main/java/com/neueda/jetbrains/plugin/graphdb/database/api/data/GraphMetadata.java
@@ -1,34 +1,8 @@
 package com.neueda.jetbrains.plugin.graphdb.database.api.data;
 
-import java.util.Collections;
 import java.util.Map;
 
-import static java.util.Collections.*;
-
 public interface GraphMetadata {
-    GraphMetadata EMPTY = new GraphMetadata() {
-
-        @Override
-        public Map<String, Number> labels() {
-            return emptyMap();
-        }
-
-        @Override
-        public Map<String, Number> relationships() {
-            return emptyMap();
-        }
-
-        @Override
-        public Iterable<String> vertexProperties() {
-            return emptyList();
-        }
-
-        @Override
-        public Iterable<String> edgeProperties() {
-            return emptyList();
-        }
-    };
-
     Map<String, Number> labels();
 
     Map<String, Number> relationships();
