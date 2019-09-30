4e9eb6a356a415a8ad30ff71532ab8cc9ca391eb
Dwitry <dwitry@users.noreply.github.com>
2019-09-30 15:44:28 +0300

    Fix browser not opening on some systems

--- ui/jetbrains/src/main/java/com/neueda/jetbrains/plugin/graphdb/jetbrains/actions/execute/LandingPageAction.java
+++ ui/jetbrains/src/main/java/com/neueda/jetbrains/plugin/graphdb/jetbrains/actions/execute/LandingPageAction.java
@@ -5,16 +5,23 @@ import com.intellij.ide.browsers.BrowserLauncher;
 import com.intellij.openapi.ui.Messages;
 import com.neueda.jetbrains.plugin.graphdb.jetbrains.component.analytics.Analytics;
 
+import java.awt.*;
 import java.net.URI;
 
 public class LandingPageAction {
+    public static final String URL = "https://google.com";
+
     public static void open() {
         Analytics.event("landingPage", "clicked");
-        int ok = Messages.showOkCancelDialog("todo https://google.com", "todo", "Read more", "Cancel",
+        int ok = Messages.showOkCancelDialog("todo " + URL, "todo", "Read more", "Cancel",
                 AllIcons.General.QuestionDialog);
         if (ok == 0) {
             Analytics.event("landingPage", "land");
-            BrowserLauncher.getInstance().browse(URI.create("https://google.com"));
+            try {
+                Desktop.getDesktop().browse(URI.create(URL));
+            } catch (Exception e) {
+                BrowserLauncher.getInstance().browse(URI.create(URL));
+            }
         } else {
             Analytics.event("landingPage", "cancel");
         }
