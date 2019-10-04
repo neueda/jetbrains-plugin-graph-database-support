4d13af3fde8258b1184e3cbb873496ab7dfb742a
Vladimir Ilyin <vladimir.ilyin@neueda.com>
2019-10-04 18:49:43 +0300

    Turn off Gremlin driver logging

--- database/opencypher/src/main/java/com/neueda/jetbrains/plugin/graphdb/database/opencypher/gremlin/OpenCypherGremlinDatabase.java
+++ database/opencypher/src/main/java/com/neueda/jetbrains/plugin/graphdb/database/opencypher/gremlin/OpenCypherGremlinDatabase.java
@@ -10,6 +10,7 @@ import com.neueda.jetbrains.plugin.graphdb.database.api.query.GraphQueryResultRo
 import com.neueda.jetbrains.plugin.graphdb.database.neo4j.bolt.data.Neo4jBoltQueryResultColumn;
 import com.neueda.jetbrains.plugin.graphdb.database.neo4j.bolt.data.Neo4jBoltQueryResultRow;
 import com.neueda.jetbrains.plugin.graphdb.database.opencypher.gremlin.query.OpenCypherGremlinQueryResult;
+import org.apache.log4j.PropertyConfigurator;
 import org.apache.tinkerpop.gremlin.driver.Client;
 import org.apache.tinkerpop.gremlin.driver.Cluster;
 import org.apache.tinkerpop.gremlin.driver.Result;
@@ -17,16 +18,12 @@ import org.neo4j.driver.v1.exceptions.ClientException;
 import org.opencypher.gremlin.client.CypherGremlinClient;
 
 import java.net.URI;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 import static java.util.Collections.*;
-import static java.util.stream.Collectors.*;
-
+import static java.util.stream.Collectors.toList;
 /**
  * Communicates with TinkerPop database by translating Cypher to Gremlin
  */
@@ -34,6 +31,10 @@ public class OpenCypherGremlinDatabase implements GraphDatabaseApi {
     private final OpenCypherGremlinValueConverter converter;
     private final Cluster cluster;
     private final GremlinFlavor flavor;
+
+    static {
+        disableGremlinLog();
+    }
 
     public OpenCypherGremlinDatabase(Map<String, String> configuration) {
         this(new OpenCypherGremlinConfiguration(configuration));
@@ -161,5 +162,11 @@ public class OpenCypherGremlinDatabase implements GraphDatabaseApi {
                 emptyList(),
                 emptyList()
         );
+    }
+
+    private static void disableGremlinLog() {
+        Properties props = new Properties();
+        props.setProperty("log4j.logger.org.apache.tinkerpop.gremlin.driver", "OFF");
+        PropertyConfigurator.configure(props);
     }
 }
