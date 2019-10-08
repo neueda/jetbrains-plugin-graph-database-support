736db0ee7dd701d7a9adc7f22fe44de43b6a23a9
Viktorija Geseltina <viktorija.geseltina@neueda.com>
2019-10-08 17:48:12 +0300

    Updated link to plugin page (#146)
    * Updated link to plugin page
    
    * Improved message

--- ui/jetbrains/src/main/java/com/neueda/jetbrains/plugin/graphdb/jetbrains/actions/execute/LandingPageAction.java
+++ ui/jetbrains/src/main/java/com/neueda/jetbrains/plugin/graphdb/jetbrains/actions/execute/LandingPageAction.java
@@ -9,11 +9,12 @@ import java.awt.*;
 import java.net.URI;
 
 public class LandingPageAction {
-    public static final String URL = "https://google.com";
+    public static final String URL = "https://technologies.neueda.com/plugin";
 
     public static void open() {
         Analytics.event("landingPage", "clicked");
-        int ok = Messages.showOkCancelDialog("todo " + URL, "todo", "Read more", "Cancel",
+        int ok = Messages.showOkCancelDialog("This feature is planned for a \nfuture release of the premium version.\n" +
+                        "If you are interested, please visit: \n\n" + URL, "Premium Version", "Find more", "Cancel",
                 AllIcons.General.QuestionDialog);
         if (ok == 0) {
             Analytics.event("landingPage", "land");
