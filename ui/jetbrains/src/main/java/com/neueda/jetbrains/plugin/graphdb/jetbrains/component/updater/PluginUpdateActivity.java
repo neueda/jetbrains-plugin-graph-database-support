527deca7b21d2ae9617d09c5a0b2652e0beae87b
Nikita R-T <nikita.rt@neueda.com>
2019-10-21 15:20:32 +0300

    Update notification (#152)
    * Update popup with new text. Deprecation warning fix
    
    * Activity instead of Component to avoid deprecation issues

---
+++ ui/jetbrains/src/main/java/com/neueda/jetbrains/plugin/graphdb/jetbrains/component/updater/PluginUpdateActivity.java
@@ -0,0 +1,44 @@
+package com.neueda.jetbrains.plugin.graphdb.jetbrains.component.updater;
+
+import com.intellij.notification.*;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupActivity;
+import com.neueda.jetbrains.plugin.graphdb.jetbrains.component.settings.SettingsComponent;
+import com.neueda.jetbrains.plugin.graphdb.jetbrains.util.PluginUtil;
+import com.neueda.jetbrains.plugin.graphdb.platform.GraphBundle;
+import com.neueda.jetbrains.plugin.graphdb.platform.GraphConstants;
+import org.jetbrains.annotations.NotNull;
+
+public class PluginUpdateActivity implements StartupActivity, DumbAware {
+
+    private static final String NOTIFICATION_ID = "GraphDatabaseSupportUpdateNotification";
+    private boolean isUpdateNotificationShown = false;
+
+    @Override
+    public void runActivity(@NotNull Project project) {
+        String currentVersion = PluginUtil.getVersion();
+        String knownVersion = SettingsComponent.getInstance().getKnownPluginVersion();
+
+        boolean isUpdated = !currentVersion.equals(knownVersion);
+        if (isUpdated || GraphConstants.IS_DEVELOPMENT) {
+            if (!isUpdateNotificationShown) {
+                SettingsComponent.getInstance().setKnownPluginVersion(currentVersion);
+                showNotification(project, currentVersion);
+                isUpdateNotificationShown = true;
+            }
+        }
+    }
+
+    private void showNotification(Project project, String currentVersion) {
+        NotificationGroup group = new NotificationGroup(NOTIFICATION_ID, NotificationDisplayType.STICKY_BALLOON, true);
+        Notification notification = group.createNotification(
+                GraphBundle.message("updater.title", currentVersion),
+                GraphBundle.message("updater.notification"),
+                NotificationType.INFORMATION,
+                new NotificationListener.UrlOpeningListener(false)
+        );
+        Notifications.Bus.notify(notification, project);
+    }
+
+}
